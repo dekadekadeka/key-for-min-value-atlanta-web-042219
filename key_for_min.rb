a6d51def1c76cbbbe22a4e53de7b8b47e63ba2ea
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-04-06 03:39:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,8 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
+cobber = {:blake => 500, :ashley => 2, :adam => 1}
+
 def key_for_min_value(name_hash)
   name_hash.collect do |name, value|
   if name_hash[0][value] < name_hash[1][value]
@@ -8,3 +10,4 @@ def key_for_min_value(name_hash)
   end
   end
 end
+key_for_min_value(cobber)
