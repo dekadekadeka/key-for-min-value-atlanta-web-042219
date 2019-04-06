3f85bf0c52c8473e8144bec657c905f0d2bcc8b9
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-04-06 03:40:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,7 @@ cobber = {:blake => 500, :ashley => 2, :adam => 1}
 
 def key_for_min_value(name_hash)
   name_hash.collect do |name, value|
-  if name_hash[0][value] < name_hash[1][value]
-    name_hash[0][name]
+  if value > value
   end
   end
 end
